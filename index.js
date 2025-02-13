9d95368d7582583c7489349e91ff552e0d756170
Cheickna01 <diabyhamala0@gmail.com>
2025-02-13 05:10:06 +0100

    kjn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 mongoose
-  .connect(`mongodb+srv://diabyhamala0:AZERhd%40001diab@cluster0.tgnse.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+  .connect(`mongodb+srv://diabyhamala0:AZERhd%40001diab@cluster0.tgnse.mongodb.net/talkify?retryWrites=true&w=majority&appName=Cluster0`)
   .then(console.log("connection à la base de donées réussie..."));
 
 const app = express();
