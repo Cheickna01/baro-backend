0b8a049ee3dba0d4ca465235a2b71c39088c4e9e
Cheickna01 <diabyhamala0@gmail.com>
2025-02-13 06:08:47 +0100

    jhaaa

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 require("dotenv").config();
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
